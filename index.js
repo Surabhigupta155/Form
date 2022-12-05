97d48b2448b2d5c8419b6303703c9ab70aa0b929
Surabhi Gupta <surabhigupta1519@gmail.com>
2022-12-05 18:17:59 +0530

    new commit

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.get('/', (req, res) => res.send('Welcome to the server'));
 const pitches = require('./routes/pitches');
 app.use('/pitches', pitches);
 
-const prt = process.env.prt || 8081;
+const PORT = process.env.PORT || 8081;
 
 // start server and listen on port 8081 for connections
-app.listen(prt, async() => {
+app.listen(PORT, async() => {
     console.log('Server up on http://localhost:8081')
     await sequelize.sync({force: true}) // synchronize sequelize model with database tables
     console.log('Database Connected!');
